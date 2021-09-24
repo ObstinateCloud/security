bc4d3940143a930c9507a03f3c6fe496210db700
zhangjianyun <zhangjianyun_kd@163.com>
2021-09-24 18:03:58 +0800

    前后端分离开发模式配置

--- src/main/java/com/lengedyun/security/config/WebConfig.java
+++ src/main/java/com/lengedyun/security/config/WebConfig.java
@@ -1,7 +1,11 @@
 package com.lengedyun.security.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -11,6 +15,8 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.PrintWriter;
 
 /**
  * @title: WebConfig
@@ -26,13 +32,48 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("zjy")
                 //密文 zjy
-//                .password("$2a$10$dIHsUce.Gmjx.xYjffI3y.JcO/18q72Oe2KeeiUsOrczyFtGqTn8W")
-                .password("zjy")
+                .password("$2a$10$dIHsUce.Gmjx.xYjffI3y.JcO/18q72Oe2KeeiUsOrczyFtGqTn8W")
+//                .password("zjy")
                 //角色一定要配
                 .roles("admin")
                     ;
     }
 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .formLogin()
+//                //如果只配置了loginPage，则前端form的action地址与loginPage一致
+//                .loginPage("/mylogin.html")
+//                //如果同时配置了loginPage和loginProcessingUrl，则前端form的action地址与loginProcessingUrl一致,否则无法跳转
+//                .loginProcessingUrl("/doLogin")
+//                .usernameParameter("user")
+//                .passwordParameter("pwd")
+//                .permitAll()
+//                //1.服务端跳转，无论从哪来都去loginSuccess
+////                .successForwardUrl("/loginSuccess")
+//                //2.客户端跳转，重定向到登录前的路径 一般1.和2.只需配置一个
+////                .defaultSuccessUrl("/defaultSuccessUrl",true)//无论如何都跳转效果和1一样
+//                .defaultSuccessUrl("/defaultSuccessUrl")
+////                .failureForwardUrl("/loginFailureForward")
+//                .failureUrl("/loginFailure")
+//                .and()
+//                .logout()
+////                .logoutUrl("/myLogout")
+////                .logoutRequestMatcher(new AntPathRequestMatcher("/myLogoutPost","POST"))
+//                .and()
+//                .csrf().disable();
+//    }
+
+    /**
+     *  前后端分离
+     * 采用handler方式处理请求
+     * @param http
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -47,20 +88,50 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
                 .usernameParameter("user")
                 .passwordParameter("pwd")
                 .permitAll()
-                //1.服务端跳转，无论从哪来都去loginSuccess
-//                .successForwardUrl("/loginSuccess")
-                //2.客户端跳转，重定向到登录前的路径 一般1.和2.只需配置一个
-//                .defaultSuccessUrl("/defaultSuccessUrl",true)//无论如何都跳转效果和1一样
-                .defaultSuccessUrl("/defaultSuccessUrl")
-//                .failureForwardUrl("/loginFailureForward")
-                .failureUrl("/loginFailure")
+                .successHandler((req,resp,auth)->{
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter writer = resp.getWriter();
+                    writer.write(new ObjectMapper().writeValueAsString(auth.getPrincipal()));
+                    writer.flush();
+                    writer.close();
+
+                })
+                .failureHandler((req,resp,excecption)->{
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter writer = resp.getWriter();
+                    String msg = "login error";
+
+                    if (excecption instanceof LockedException){
+                        msg = "account lock";
+                    }else if (excecption instanceof BadCredentialsException){
+                        msg = "account or password error";
+                    }
+                    writer.write(new ObjectMapper().writeValueAsString(msg));
+                    writer.flush();
+                    writer.close();
+                })
                 .and()
                 .logout()
-//                .logoutUrl("/myLogout")
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/myLogoutPost","POST"))
+                .logoutSuccessHandler((req,resp,auth)->{
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter writer = resp.getWriter();
+                    writer.write(new ObjectMapper().writeValueAsString("注销登录成功"));
+                    writer.flush();
+                    writer.close();
+                })
                 .and()
-                .csrf().disable();
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint((req,resp,exception)->{
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter writer = resp.getWriter();
+                    writer.write(new ObjectMapper().writeValueAsString("未登录"));
+                    writer.flush();
+                    writer.close();
+                })
+        ;
     }
+
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -69,8 +140,8 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
+//        return NoOpPasswordEncoder.getInstance();
     }
 
     public static void main(String[] args) {
